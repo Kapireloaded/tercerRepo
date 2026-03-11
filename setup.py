030adf037a15550cc58e8a629a2fc3a0ee9369f7
kapireloaded <kapireloaded@gmail.com>
2026-03-11 00:14:51 -0600

    mi primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                                    # Nombre del paquete
+    version="0.1.0",                                      # Versión inicial
+    packages=find_packages(),                             # Paquetes a incluir
+    description="Un paquete pip simple de saludo",        # Breve descripción
+    author="Jose Luis Rodriguez Romero",                  # Tu nombre
+    author_email="kapireloaded@gmail.com",                # Tu correo electrónico
+    url="https://github.com/kapireloaded/tercerRepo",     # URL del proyecto
+)
